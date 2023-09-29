7c414c66c7abfdac306d4c6e2a2344189527c140
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-09-29 15:48:53 -0500

    Remove dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!bin/bash
 
 ./mvnw clean package
+docker build -f src/main/docker/Dockerfile.native -t quarkus/backend_capcol .
+docker run -i --rm --name backend_capcol --env PORT=8081 -p 8081:8081 quarkus/backend_capcol

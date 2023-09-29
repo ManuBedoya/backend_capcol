e50bce1abea41550322af574233d2382e53fbea5
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-09-29 15:57:03 -0500

    Add target

---
+++ target/classes/import.sql
@@ -0,0 +1,7 @@
+--INSERT INTO users(id, identification, name, username, password) VALUES (1, '10512582', 'Manuel', 'Manu98', '12345');
+--INSERT INTO users(id, identification, name, username, password) VALUES (2, '2544521', 'Salome', 'Salome98', '12345');
+--INSERT INTO users(id, identification, name, username, password) VALUES (3, '354544', 'Maria', 'Maria98', '12345');
+
+--INSERT INTO products(id, identification, name, ammount, price, applyVariants) VALUES (1, '001', 'Cafetera', 5, 200000, false);
+--INSERT INTO products(id, identification, name, ammount, price, applyVariants) VALUES (2, '002', 'Termo', 20, 50000, false);
+--INSERT INTO products(id, identification, name, ammount, price, applyVariants) VALUES (3, '003', 'Tablero', 150, 15000, false);
